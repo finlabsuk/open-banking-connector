2eade76dee7f658f7e0ca107081d547ab9dec2e5
Mark Murphy <26673915+markm77@users.noreply.github.com>
2023-09-18 16:06:40 +0100

    Prepare for new model bank UI methods

--- test/OpenBanking.Library.Connector.BankTests/BrowserInteraction/BankUiMethods/ObieUiMethods.cs
+++ test/OpenBanking.Library.Connector.BankTests/BrowserInteraction/BankUiMethods/ObieUiMethods.cs
@@ -22,45 +22,48 @@ public class ObieUiMethods : IBankUiMethods
         IPage page,
         BankUser bankUser)
     {
-        await page.GetByLabel("* User Name").ClickAsync();
-
-        await page.GetByLabel("* User Name").FillAsync(bankUser.UserNameOrNumber);
-
-        await page.GetByLabel("* Password").ClickAsync();
-
-        await page.GetByLabel("* Password").FillAsync(bankUser.Password);
-
-        await page.GetByRole(
-            AriaRole.Link,
-            new PageGetByRoleOptions { NameString = "Login" }).ClickAsync();
-
-        if (consentVariety == ConsentVariety.AccountAccessConsent)
+        if (_obieBank is ObieBank.Modelo)
         {
-            await page.GetByRole(
-                    AriaRole.Listbox,
-                    new PageGetByRoleOptions { NameString = "* Accounts" })
-                .SelectOptionAsync(new[] { "700004000000000000000003" });
+            await page.GetByLabel("* User Name").ClickAsync();
 
-            // Seems necessary for next click to register
-            await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+            await page.GetByLabel("* User Name").FillAsync(bankUser.UserNameOrNumber);
+
+            await page.GetByLabel("* Password").ClickAsync();
+
+            await page.GetByLabel("* Password").FillAsync(bankUser.Password);
 
             await page.GetByRole(
                 AriaRole.Link,
-                new PageGetByRoleOptions { NameString = "Confirm" }).ClickAsync();
-        }
-        else
-        {
-            await page.GetByRole(
-                    AriaRole.Combobox,
-                    new PageGetByRoleOptions { NameString = "* Select debtor account" })
-                .SelectOptionAsync(new[] { "700004000000000000000002" });
+                new PageGetByRoleOptions { NameString = "Login" }).ClickAsync();
 
-            // Seems necessary for next click to register
-            await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+            if (consentVariety == ConsentVariety.AccountAccessConsent)
+            {
+                await page.GetByRole(
+                        AriaRole.Listbox,
+                        new PageGetByRoleOptions { NameString = "* Accounts" })
+                    .SelectOptionAsync(new[] { "700004000000000000000003" });
 
-            await page.GetByRole(
-                AriaRole.Link,
-                new PageGetByRoleOptions { NameString = "Confirm" }).ClickAsync();
+                // Seems necessary for next click to register
+                await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+
+                await page.GetByRole(
+                    AriaRole.Link,
+                    new PageGetByRoleOptions { NameString = "Confirm" }).ClickAsync();
+            }
+            else
+            {
+                await page.GetByRole(
+                        AriaRole.Combobox,
+                        new PageGetByRoleOptions { NameString = "* Select debtor account" })
+                    .SelectOptionAsync(new[] { "700004000000000000000002" });
+
+                // Seems necessary for next click to register
+                await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+
+                await page.GetByRole(
+                    AriaRole.Link,
+                    new PageGetByRoleOptions { NameString = "Confirm" }).ClickAsync();
+            }
         }
     }
 }
