cd96eb181d5448eca3d2c8c18566b26f6d5f0c7e
Mark Murphy <26673915+markm77@users.noreply.github.com>
2026-01-17 22:22:05 +0000

    [VRP] Update Nationwide

--- src/OpenBanking.Library.Connector/BankProfiles/Generators/NationwideGenerator.cs
+++ src/OpenBanking.Library.Connector/BankProfiles/Generators/NationwideGenerator.cs
@@ -159,7 +159,8 @@ public class NationwideGenerator : BankProfileGeneratorBase<NationwideBank>
                 DomesticVrpConsent = new DomesticVrpConsentCustomBehaviour
                 {
                     PostResponseLinksMayOmitId = true,
-                    PreferMisspeltContractPresentIndicator = true
+                    PreferMisspeltContractPresentIndicator = true,
+                    ResponseLinksMayHaveIncorrectUrlBeforeQuery = true
                 },
                 DomesticVrp = new DomesticVrpCustomBehaviour
                 {
